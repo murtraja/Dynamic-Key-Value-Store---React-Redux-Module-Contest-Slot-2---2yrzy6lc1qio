c8381c325e99f83c679140bbad038825e9f7c6c4
Murtaza Raja <murt.raja@gmail.com>
2024-02-05 23:23:39 +0530

    functionality done

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,19 @@
-import React from 'react';
-import { BrowserRouter as Router, Link } from 'react-router-dom';
-import KeyValueStore from './KeyValueStore';
-import '../styles/App.css';
+import React from "react";
+import { BrowserRouter as Router, Link } from "react-router-dom";
+import KeyValueStore from "./KeyValueStore";
+import "../styles/App.css";
 
 function App() {
   return (
     <Router>
       <div id="main">
         <nav>
-          Reset
+          <Link to="/">Reset</Link>
         </nav>
         <KeyValueStore />
       </div>
     </Router>
   );
 }
-
 
 export default App;
--- src/components/KeyValueStore.js
+++ src/components/KeyValueStore.js
@@ -1,39 +1,90 @@
-import React, { useState, useEffect } from 'react';
-import '../styles/App.css';
+import React, { useState, useEffect } from "react";
+import {
+  BrowserRouter as Router,
+  useLocation,
+  useHistory,
+} from "react-router-dom";
+
+import "../styles/App.css";
 
 const KeyValueStore = () => {
-  
-    const [keyValues, setKeyValues] = useState({});
-  
-    const updateKeyValue = (key, value) => {};
-  
-    const handleUpdateClick = () => {};
-  
-    const handleDeleteClick = (key) => {};
-  
-    useEffect(() => {}, []);
-  
-    return (
+  const [keyValues, setKeyValues] = useState({});
+  const history = useHistory();
+  const { search } = useLocation();
+  const parseKeyValuesFromURL = () => {
+    const searchParams = new URLSearchParams(search);
+    const keyValues = {};
+    searchParams.forEach((value, key) => {
+      keyValues[key] = value;
+    });
+    setKeyValues(keyValues);
+  };
+
+  const updateKeyValue = (key, value) => {};
+
+  const getQueryStringFromKeyValues = (keyValues) => {
+    const searchParams = new URLSearchParams();
+    for (let key in keyValues) {
+      searchParams.append(key, keyValues[key]);
+    }
+    console.log(searchParams.toString());
+    return searchParams.toString();
+  };
+
+  const handleUpdateClick = () => {
+    const queryString = getQueryStringFromKeyValues(keyValues);
+    const url = `/?${queryString}`;
+    history.push(url);
+  };
+
+  const handleDeleteClick = (key) => {
+    const newKeyValues = { ...keyValues };
+    delete newKeyValues[key];
+    const queryString = getQueryStringFromKeyValues(newKeyValues);
+    const url = `/?${queryString}`;
+    history.push(url);
+  };
+
+  useEffect(() => {
+    parseKeyValuesFromURL();
+  }, [search]);
+  const key = 3;
+  return (
+    <div>
+      <h1>Key Value Store</h1>
       <div>
-        <h1>Key Value Store</h1>
-          <div>
-            {/* Map the key-value using the below HTML */}
-              <div key={key} className='key-value-div'>
-                <span className='key-field'>key:</span>
-                <input
-                    className='value-field'
-                    type="text"
-                />
-                <button className='delete-btn'>Delete</button>
-              </div>
-
-            {/* Do not include Update button, while mapping the key-value */}
-            <button className='update-btn'>Update Values</button>
+        {Object.keys(keyValues).map((key) => (
+          <div key={key} className="key-value-div">
+            <span className="key-field">{key}:</span>
+            <input
+              className="value-field"
+              type="text"
+              value={keyValues[key]}
+              onChange={(evt) => {
+                const value = evt.target.value;
+                setKeyValues((prev) => ({ ...prev, [key]: value }));
+              }}
+            />
+            <button
+              className="delete-btn"
+              onClick={() => handleDeleteClick(key)}
+            >
+              Delete
+            </button>
           </div>
+        ))}
 
-          <p>No key values found in URL.</p>
+        {/* Do not include Update button, while mapping the key-value */}
+        <button className="update-btn" onClick={handleUpdateClick}>
+          Update Values
+        </button>
       </div>
-    )
-}
+
+      {Object.keys(keyValues).length === 0 ? (
+        <p>No key values found in URL.</p>
+      ) : null}
+    </div>
+  );
+};
 
 export default KeyValueStore;
